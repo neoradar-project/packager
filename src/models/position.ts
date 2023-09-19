63468484be3fb6afdfe8b4b85c79e14674dfa630
Pierre Ferran <pierr3@users.noreply.github.com>
2023-09-19 18:58:52 +0200

    Build position callsign correctly per ESE definition

--- src/models/position.ts
+++ src/models/position.ts
@@ -53,19 +53,27 @@ export class PackageAtcPosition {
                 }
             }
 
-            const gate = new PackageAtcPosition({
-                callsign: data[0].replace("\r", ""),
+            let sector = data[5].replace("\r", "");
+            let subSector = data[4].replace("\r", "").replace("-", ""); // Remove - which should be null per RFC
+            let identifier = data[3].replace("\r", "");
+            let facility = data[6].replace("\r", "");
+
+            let callsign = sector + "_" + subSector + "_" + facility;
+            callsign = callsign.replace("__", ""); // In case subsector is blank
+
+            const out = new PackageAtcPosition({
+                callsign: callsign,
                 name: data[1].replace("\r", ""),
                 frequency: data[2].replace("\r", ""),
-                identifier: data[3].replace("\r", ""),
-                subSector: data[4].replace("\r", ""),
-                sector: data[5].replace("\r", ""),
-                facility: data[6].replace("\r", ""),
+                identifier: identifier,
+                subSector: subSector,
+                sector: sector,
+                facility: facility,
                 squawkStart: data[9].replace("\r", ""),
                 squawkEnd: data[10].replace("\r", ""),
                 visiblityPoints: allPoints
             })
-            return gate
+            return out
         }
         return null
     }
